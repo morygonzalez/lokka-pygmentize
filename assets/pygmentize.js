157a0da4a8dc5e91d7a8b7d27ec7ece90d4ffdfa
森井ゴンザレス <morygonzalez@gmail.com>
2012-09-18 13:18:43 +0900

    Removes old code

--- assets/pygmentize.js
+++ assets/pygmentize.js
@@ -11,39 +11,6 @@
         $(self).replaceWith(data);
       });
     });
-
-    /*
-    $('.body').each(function() {
-
-      var Pygmentize = function(lexar, snippet) {
-        var result;
-
-        $.ajax({
-          type: 'POST',
-          url: '/pygmentize',
-          async: false,
-          data: {
-            lexar: lexar,
-            snippet: snippet
-          },
-        }).done(function(data) {
-          result = data;
-        });
-
-        return result;
-      }
-
-      var entryBody = $(this).text();
-
-      entryBody = entryBody.replace(/```(.+?)\n([\s\S]*?)```/g,
-        function(whole, lexar, snippet) {
-          return Pygmentize(lexar, snippet);
-        }
-      );
-
-      $(this).html(entryBody);
-    })
-    */
   }
 
   document.body.addEventListener('AutoPagerize_DOMNodeInserted',function(evt){
