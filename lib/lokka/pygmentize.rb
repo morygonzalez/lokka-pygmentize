c4fa293fbc7dfdf2bbbe98c1bbdb075ce7cf42b0
森井ゴンザレス <morygonzalez@gmail.com>
2013-02-04 00:16:37 +0900

    Also deal Entry#short_body, Use around alias

--- lib/lokka/pygmentize.rb
+++ lib/lokka/pygmentize.rb
@@ -5,7 +5,6 @@ module Lokka
     def self.registered(app)
       app.before do
         assets_path = "/plugin/lokka-pygmentize/assets"
-
         content_for :header do
           text = <<-EOS.strip_heredoc
           <link href="#{assets_path}/monokai.css" rel="stylesheet" type="text/css" />
@@ -17,15 +16,31 @@ module Lokka
 end
 
 class Entry
-  def body
-    doc  = Nokogiri::HTML(self.long_body)
+  alias_method :original_long_body, :body
+  def highlighted_long_body
+    syntax_highlight(self.original_long_body)
+  end
+  alias_method :body, :highlighted_long_body
+
+  alias_method :original_short_body, :short_body
+  def highlighted_short_body
+    syntax_highlight(self.original_short_body)
+  end
+  alias_method :short_body, :highlighted_short_body
+
+  def syntax_highlight(body)
+    doc = Nokogiri::HTML(body)
     doc.search("//pre").each do |pre|
       code = pre.css("code")[0]
-      pre.replace Pygments.highlight(
-        code.text.rstrip,
-        :lexer   => code[:class],
-        :options => { :encoding => 'utf-8' }
-      )
+      begin
+        pre.replace Pygments.highlight(
+          code.text.rstrip,
+          :lexer   => pre[:class].present? ? pre[:class] : nil,
+          :options => { :encoding => 'utf-8' }
+        ) if code
+      rescue MentosError
+        next
+      end
     end
     doc.to_s
   end
