aa12a64178f8437054fbd00c0e3371ee6586e281
森井ゴンザレス <morygonzalez@gmail.com>
2014-07-13 16:11:06 +0900

    Stop bundling assets

--- lib/lokka/pygmentize.rb
+++ lib/lokka/pygmentize.rb
@@ -3,14 +3,6 @@ require 'pygments.rb'
 module Lokka
   module Pygmentize
     def self.registered(app)
-      app.before do
-        assets_path = "/plugin/lokka-pygmentize/assets"
-        content_for :header do
-          text = <<-EOS.strip_heredoc
-          <link href="#{assets_path}/monokai.css" rel="stylesheet" type="text/css" />
-          EOS
-        end
-      end
     end
   end
 end
